dd7f540c3fdde4f64c8d4f9eda4236b2ec84a40d
Death Gun24 <miguel_guzman6@usmp.pe>
2025-10-02 17:56:19 -0500

    cambios miguel

--- models/marts/dimensions/dim_customer.sql
+++ models/marts/dimensions/dim_customer.sql
@@ -1,3 +1,8 @@
+-- Este proceso ETL unifica la información de clientes tomando datos de la tabla staging (stg_customer) 
+-- y los enriquece con detalles de dirección, ciudad y país desde las tablas fuente (dvdrental). 
+-- El resultado es una vista consolidada de clientes con datos personales, de contacto y localización.
+
+
 with customers as (
     select * from {{ ref('stg_customer') }}
 ),
